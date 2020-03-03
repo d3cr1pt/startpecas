8a5c434cced94d73a3edad4e06b3f772015cf845
Alberto Trevisan <mineplay152@gmail.com>
2020-03-03 19:13:23 -0300

    Organização do Menu Principal

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = {
+    pages: {
+        index: {
+            filename: 'index.html',
+            title_ptBR: 'Início'
+        },
+        shop: {
+            filename: 'shop.html',
+            title_ptBR: 'Loja'
+        },
+        about: {
+            filename: 'about.html',
+            title_ptBR: 'Sobre Nós'
+        },
+        contact: {
+            filename: 'contact.html',
+            title_ptBR: 'Contato'
+        },
+        services: {
+            filename: 'services.html',
+            title_ptBR: 'Serviços'
+        }
+    }
+}
